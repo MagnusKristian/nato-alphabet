aaab90bbd79101f860bc97325c2d459ea809214d
MagnusKristian <92784711+MagnusKristian@users.noreply.github.com>
2022-05-16 14:28:25 +0200

    upload

---
+++ Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kodeMedLinn
+{
+    internal class Class1
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public int Age { get; set; }
+
+        public Class1(string name,string description,int age)
+        {
+            Name = name;
+            Description = description;
+            Age = age;
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace kodeMedLinn // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //var m = new Class1("Magnus","kjører motorsykkel",25);
+            //Console.WriteLine(m.Name);
+
+            string[] alphabet = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "æ", "ø", "å" };
+            string[] words = { "ALFA", "BRAVO", "CHARLIE", "DELTA", "ECHO", "FOXTROT", "GOLF", "HOTEL", "INDIA", "JULIET", "KILO", "LIMA", "MIKE", "NOVEMBER", "OSCAR", "PAPA", "QUEBEC", "ROMEO", "SIERRA", "TANGO", "UNIFORM", "VICTOR", "WHISKEY", "X-RAY", "YANKEE", "ZULU", "ÆRLIG", "ØSTEN", "ÅSE" };
+            var random = new Random();
+            int score = 0;
+       
+            Console.WriteLine("Natophonetic alphabet:");
+            Console.WriteLine("Heres a letter, what is its natophonetic word?");
+            
+            int counter = 0;
+            int rightCounter = 0;
+            int wrongCounter = 0;
+            
+            while (true)
+            {
+                int randomNumber = random.Next(0, alphabet.Length + 1);
+                string correctAnswer = words[randomNumber];
+                Console.WriteLine($"Her er bokstaven: {alphabet[randomNumber]}");
+                //Console.WriteLine("riktig svar er: "+ correctAnswer);
+                counter++;                
+                string userInput = Console.ReadLine().ToUpper();
+                
+                if ( userInput == correctAnswer)
+                {
+                    Console.WriteLine("RIKTIG");
+                    score++;
+                    rightCounter++;
+                    Console.WriteLine($"Your score is: {score}");
+                    Console.WriteLine("End:-----------------------------------------");
+                }
+                else if(userInput == "STOP") 
+                    { Console.WriteLine("Goodbye, thanks for playing."); break; }
+
+                else if (counter == 10) 
+                    { Console.WriteLine($"Game is finished. You got {rightCounter} right, and {wrongCounter} wrong. Your score is {score}."); break; }
+
+                else 
+                { 
+                    Console.WriteLine($"{userInput} ER FEIL");
+                    score--; 
+                    wrongCounter++;
+                    Console.WriteLine($"Your score is: {score}");
+                    Console.WriteLine("End:-----------------------------------------");
+                }
+
+            }
+            
+
+
+        }
+    }
+}
